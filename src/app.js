40972dba043c4e38feddef3dfc131d6e68849bc7
abhijit.bhadoria <abhijit.bhadoria@bfhl.in>
2024-09-25 19:31:28 +0530

    http method routes

--- src/app.js
+++ src/app.js
@@ -2,19 +2,40 @@ const express = require("express");
 
 const app = express();
 
-app.use("/ping",(req, res) => {
-  res.send("response from server");
+app.get("/user", (req, res) => {
+  res.send({
+    "firstName": "Abhijit",
+    "lastName": "Bhadoria"
+  });
 });
 
-app.use("/hello",(req, res) => {
-  res.send("Hello from server");
+app.post("/user", (req, res) => {
+  res.send("User saved in DB");
 });
 
-app.use("/test",(req, res) => {
-  res.send("mic testing 1..2..4..");
+app.delete("/user", (req,res) => {
+  res.send("User deleted from DB");
 });
+
+// app.use("/", (req, res) => {
+//   res.send("Namaste Abhijit");
+// });
+
+// app.use("/ping",(req, res) => {
+//   res.send("response from server");
+// });
+
+// app.use("/hello",(req, res) => {
+//   res.send("Hello from server");
+// });
+
+// app.use("/test",(req, res) => {
+//   res.send("mic testing 1..2..4..");
+// });
 
 app.listen(3000, ()=>{
   console.log("Server is running on port 3000...");
 });
+
+
 
