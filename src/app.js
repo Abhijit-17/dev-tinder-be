85d920a761dbfe2bf7199cd00175737e5d3ec261
abhijit.bhadoria <abhijit.bhadoria@bfhl.in>
2024-09-28 16:33:42 +0530

    init db + inserting data using api

--- src/app.js
+++ src/app.js
@@ -2,7 +2,29 @@ const express = require("express");
 
 const { connectDB } = require("./config/database");
 
+const { User } = require("./models/user");
+
 const app = express();
+
+// POST API for signup
+app.post("/signup", async (req,res) => {
+  const userObj = {
+    firstName : "Rohit",
+    lastName : "Sharma",
+    emailId : "rohit@sharma.com",
+    password : "rohit@123"
+  }
+
+  const user = new User(userObj);
+
+  try {
+    await user.save();
+    res.status(201).send("User created successfully");
+  } catch (error) {
+    res.status(400).send("Error is user creation: " + error.message);
+  }
+
+});
 
 connectDB()
 .then(() => {
