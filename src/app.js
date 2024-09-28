4e05ff17dfbcf99672104879edef6cca6242d27a
abhijit.bhadoria <abhijit.bhadoria@bfhl.in>
2024-09-28 17:54:18 +0530

    get users by email ID + feed api

--- src/app.js
+++ src/app.js
@@ -26,6 +26,37 @@ app.post("/signup", async (req,res) => {
 
 });
 
+//get API to search users by email id
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users  = await User.find({emailId : userEmail});
+    if(users.length === 0) {
+      res.status(404).send("User not found");
+    }
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong " + "\n" +error.message)
+  }
+
+});
+
+// API to get all the users - feed API
+app.get("/feed", async (req,res) => {
+  try {
+    const users  = await User.find({});
+    if(users.length === 0) {
+      res.status(404).send("No Users found");
+    }
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong " + "\n" +error.message)
+  }
+})
+
+
+
 connectDB()
 .then(() => {
   console.log("Connection to devTinder DB establish succcessfuly...")
