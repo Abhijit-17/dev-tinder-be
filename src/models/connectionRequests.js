594a08d7f84184344a96d549bf11799ebe2f3acb
abhijit.bhadoria <abhijit.bhadoria@bfhl.in>
2024-10-12 17:03:51 +0530

    compound index

--- src/models/connectionRequests.js
+++ src/models/connectionRequests.js
@@ -20,6 +20,8 @@ const connectionRequestSchema = mongoose.Schema({
   timestamps: true, optimisticConcurrency: true
 });
 
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
 connectionRequestSchema.pre("save", function(next) {
   const connectionRequest = this;
   if (connectionRequest.toUserId.equals(connectionRequest.fromUserId))
