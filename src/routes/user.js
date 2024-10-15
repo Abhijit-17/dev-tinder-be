4c2c5ad9fb15fa14c02b133234d354c7f2dea93d
abhijit.bhadoria <abhijit.bhadoria@bfhl.in>
2024-10-15 09:23:58 +0530

    /user/connections - GET API

--- src/routes/user.js
+++ src/routes/user.js
@@ -30,6 +30,73 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   }
 });
 
+userRouter.get("/user/connections", userAuth, async(req, res) => {
+  try {
+
+    const loggedInUser = req.user;
+    // const connectionRequestsFromUser = await ConnectionRequest.find({
+    //   toUserId: loggedInUser._id, 
+    //   status: "accepted"
+    // }).populate(
+    //   "fromUserId",
+    //   "firstName lastName photoUrl about age gender skills"
+    // );
+
+    // const connectionRequestsToUser = await ConnectionRequest.find({
+    //   fromUserId: loggedInUser._id,
+    //   status: "accepted"
+    // }).populate(
+    //   "toUserId",
+    //   "firstName lastName photoUrl about age gender skills"
+    // );
+
+    // const [connectionRequestsFromUser, connectionRequestsToUser] = await Promise.all([
+    //   ConnectionRequest.find({
+    //     toUserId: loggedInUser._id, 
+    //     status: "accepted"
+    //   }).populate(
+    //     "fromUserId",
+    //     "firstName lastName photoUrl about age gender skills"
+    //   ),
+    //   ConnectionRequest.find({
+    //     fromUserId: loggedInUser._id,
+    //     status: "accepted"
+    //   }).populate(
+    //     "toUserId",
+    //     "firstName lastName photoUrl about age gender skills"
+    //   )
+    // ]);
+
+    const allConnectionRequests = await ConnectionRequest.find({
+      $or: [
+        {toUserId: loggedInUser._id, status: "accepted"},
+        {fromUserId: loggedInUser._id, status: "accepted"}
+      ]
+    }).populate("fromUserId", "firstName lastName photoUrl about age gender skills")
+      .populate("toUserId", "firstName lastName photoUrl about age gender skills");
+
+    const data = allConnectionRequests.map(connectionRequest => {
+      if(loggedInUser._id.equals(connectionRequest.toUserId._id)) {
+        return connectionRequest.fromUserId
+      } else {
+        return connectionRequest.toUserId
+      }
+    });
+
+    res.json({
+      "message": "People who are connected with you",
+      "is_success": true,
+      "data": data
+    });
+
+  } catch (error) {
+    res.json({
+      "message": error.message,
+      "is_success": false
+    });
+  }
+});
+
 module.exports = {
   userRouter
 }
