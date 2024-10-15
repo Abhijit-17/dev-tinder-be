a4ece9f19c583d2c1029f25e9a31e26bfd83d27b
abhijit.bhadoria <abhijit.bhadoria@bfhl.in>
2024-10-15 10:39:58 +0530

    /user/feed - GET API

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
 const { ConnectionRequest } = require("../models/connectionRequests");
+const { User } = require("../models/user");
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -93,6 +94,43 @@ userRouter.get("/user/connections", userAuth, async(req, res) => {
     res.json({
       "message": error.message,
       "is_success": false
+    });
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async(req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const allUserConnections = await ConnectionRequest.find({
+      $or: [
+        {fromUserId: loggedInUser._id},
+        {toUserId: loggedInUser._id}
+      ]
+    }).select("fromUserId toUserId");
+
+    const feedExclusionList = new Set();
+    allUserConnections.forEach(connection => {
+      feedExclusionList.add(connection.fromUserId.toString());
+      feedExclusionList.add(connection.toUserId.toString());
+    });
+
+    const feedList = await User.find({
+      _id: {
+        $nin: Array.from(feedExclusionList)
+      }
+    }).select("firstName lastName age gender photoUrl about skills");
+
+    res.json({
+      "message": "Find new friends",
+      "is_success": true,
+      "data": feedList
+    });
+
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+      is_success: false
     });
   }
 });
